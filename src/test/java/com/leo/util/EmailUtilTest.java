1e071ee92197f9133c81c622a0dc9aaa1047522b
leo <294864460@qq.com>
2016-10-23 10:46:32 +0800

    1.剔除一些报错的信息

--- src/test/java/com/leo/util/EmailUtilTest.java
+++ src/test/java/com/leo/util/EmailUtilTest.java
@@ -13,9 +13,9 @@ public class EmailUtilTest {
     @Ignore
     public void testMail() {
         EmailUtil se   = new EmailUtil(false);
-        String    path = TestUtil.path + "ali.gif";
-        se.doSendHtmlEmail("邮件主题", "邮件内容", "438558488@qq.com");
+        String path = TestUtil.path + "ali.gif";
+        se.doSendHtmlEmail("邮件主题", "邮件内容", "294864460@qq.com");
         File affix = new File(path);
-        se.doSendHtmlEmail("邮件主题", "邮件内容", "438558488@qq.com", affix);//
+        se.doSendHtmlEmail("邮件主题", "邮件内容", "294864460@qq.com", affix);//
     }
 }
